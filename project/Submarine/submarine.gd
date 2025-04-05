bbfb114b96702e8e6525cd9f7ebd215fdc13ea29
Camden Hovell <camdenhovell@gmail.com>
2025-04-04 21:46:57 -0400

    Add submarine and ocean scenes

---
+++ project/Submarine/submarine.gd
@@ -0,0 +1,21 @@
+@icon("res://Submarine/TEMP_submarine.png")
+class_name Submarine
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
