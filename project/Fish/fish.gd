4ae8716df4b8360a51689956328fc5004ffcb8da
Camden Hovell <camdenhovell@gmail.com>
2025-04-06 15:16:07 -0400

    Add fish swimming animation

--- project/Fish/fish.gd
+++ project/Fish/fish.gd
@@ -4,11 +4,32 @@ extends CharacterBody2D
 
 var fishType : FishType = null
 
+var isUp := false
+
 @onready var sprite := %Sprite as Sprite2D
+@onready var swim_timer := %SwimTimer as Timer
 
 func _ready() -> void:
 	sprite.texture = fishType.sprite
+	swim_timer.wait_time = randf_range(0.5, 1.5)
+	swim_timer.start()
 
 
 func load_type(newFishType : FishType) -> void:
 	fishType = newFishType
+	
+
+func _on_swim_timer_timeout() -> void:
+	swim_timer.stop()
+	var tween := create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN)
+	var newPosition : Vector2
+	if isUp:
+		newPosition = Vector2(global_position.x - 50, global_position.y)
+		isUp = false
+	else:
+		newPosition = Vector2(global_position.x + 50, global_position.y)
+		isUp = true
+	tween.tween_property(self, "global_position", newPosition, 1)
+	await tween.finished
+	swim_timer.wait_time = randf_range(0.5, 1.5)
+	swim_timer.start()
